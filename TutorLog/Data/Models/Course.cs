72aa5efa1743cb475f37ee5ef77bca070db1be45
Conrad <conrad.lewin@gmail.com>
2020-03-11 15:54:06 -0400

    init commit

--- TutorLog/Data/Models/Course.cs
+++ TutorLog/Data/Models/Course.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TutorLog.Data.Models
 {
-    class Course
+    public class Course
     {
         public int ID { get; set; }
         public string Name { get; set; }
